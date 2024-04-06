bcbd8b4eb5d0b7397906bc380968e09890785199
ismailceler <17652410+Ismailceler@users.noreply.github.com>
2024-04-07 00:06:14 +0200

    Some little cleanup

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = word_count(text)
-    count2 = letter_count(text)
-    report(count2, book_path, count)
+    word_count = word_counter(text)
+    letter_dic = letter_count(text)
+    sorted_dic = sort_dic(letter_dic)
+    report(book_path, word_count, sorted_dic)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def word_count(text):
+def word_counter(text):
     words = text.split()
     return len(words)
 
@@ -29,7 +30,7 @@ def letter_count(text):
 def sort_on(dict):
     return dict["num"]
     
-def report(dic, path, count):
+def sort_dic(dic):
     list_of_dic = []
     for key in dic:
         new_dic = {}
@@ -38,11 +39,14 @@ def report(dic, path, count):
         list_of_dic.append(new_dic)
 
     list_of_dic.sort(reverse=True, key=sort_on)
+    return list_of_dic
+
+def report(path, count, dic):
     print('--- Begin report of {0} ---'.format(path))
     print('{0} words found in the document'.format(count))
-    for dic in list_of_dic:
-        if dic['char'].isalpha():
-            print('The {0} character was found {1} times'.format(dic['char'], dic['num']))
+    for d in dic:
+        if d['char'].isalpha():
+            print('The {0} character was found {1} times'.format(d['char'], d['num']))
 
     print('--- End report ---')
         
